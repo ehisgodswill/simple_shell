0190323c3cf2a48fb9017eb753edb6513bcc8c27
ehisgodswill <ehisgodswill@gmail.com>
2023-11-02 07:55:35 +0100

    get line

--- main.c
+++ main.c
@@ -2,9 +2,36 @@
 
 /**
  * main - Entry point;
- * Return: Always 0
-*/
-void int main(int argc,char **argv){
+ * Return: 0 on success, -1 on fail
+ */
+int main(void)
+{
+	/**/ size_t bufsize = BUFSIZ;
+	ssize_t char_read;
+	char *path;
+	char *buffer;
+
+	while (1)
+	{
+		buffer = (char *)malloc(sizeof(char) * bufsize);
+		if (buffer == NULL)
+		{
+			perror("Unable to allocate buffer\n");
+			exit(-1);
+		}
+		path = getcwd(buffer, PATH_MAX);
+		if (path == NULL)
+			return (-1);
+
+		printf("%s>", path);
+		char_read = getline(&buffer, &bufsize, stdin);
+		if (char_read == -1)
+			return (-1);
+
+		printf("%s", buffer);
+		free(buffer);
+		free(path);
+	}
 
 	return (0);
 }
