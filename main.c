68174fbb393eb84bbf3422eca110a9fc7124edb4
ehisgodswill <ehisgodswill@gmail.com>
2023-11-02 02:25:53 +0100

    template

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * main - Entry point;
+ * Return: Always 0
+*/
+void int main(int argc,char **argv){
+
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_SHELL
+#define _MAIN_SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
