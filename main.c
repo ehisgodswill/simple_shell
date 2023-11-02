9c31a0769879ffc860b7139605d06d6bfb83340e
ehisgodswill <ehisgodswill@gmail.com>
2023-11-02 11:24:06 +0100

    here

--- main.c
+++ main.c
@@ -1,46 +1,69 @@
 #include "main.h"
 
 /**
- * main - Entry point;
- * Return: 0 on success, -1 on fail
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	/*variable declaration*/
 	size_t bufsize = BUFSIZ;
 	ssize_t char_read;
-	char *path;
-	char *buffer;
+	char *temp, *buffer, *token;
+	int num = 0;
+	char *s = " ";
 
+	(void)argc;
+	(void)argv;
 	/*an infinite loop, that runs until exit*/
 	while (1)
 	{
-		/**
-		 * $buffer temporarily stores the current working directory
-		 * and stores it in $path variable
-		 */
 		buffer = (char *)malloc(sizeof(char) * bufsize);
 		if (buffer == NULL)
 		{
-			perror("Unable to allocate buffer\n");
+			perror("Allocate buffer failed\n");
 			exit(-1);
 		}
-		path = getcwd(buffer, PATH_MAX);
-		if (path == NULL)
+		temp= getcwd(buffer, PATH_MAX);
+		if (temp == NULL)
 			return (-1);
 
 		printf("#cisfun$ ");
-		/*$buffer now stores the shring input*/
 		char_read = getline(&buffer, &bufsize, stdin);
 		if (char_read == -1)
 			return (-1);
 		if (char_read == 1)
 			continue;
-
-		printf("%s", buffer);
+		temp = malloc(sizeof(char) * char_read);
+		if (temp == NULL)
+		{
+			perror("Allocate buffer failed\n");
+			exit(-1);
+		}
+		strcpy(temp, buffer);
+		token = strtok(temp, s);
+		num = 0;
+		while (token != NULL)
+		{
+			num++;
+			token = strtok(NULL, s);
+		}
+		strcpy(temp, buffer);
+		token = strtok(temp, s);
+		argv = malloc(sizeof (char *) * num);
+		for (num = 0; token != NULL; num++)
+		{
+			argv[num] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[num], token);
+			token = strtok(NULL, s);
+		}
+		argv[num] = NULL;
+		free(buffer);
+		free(temp);
+		free(token);
 	}
-	free(buffer);
-	free(path);
 
 	return (0);
 }
--- main.h
+++ main.h
@@ -5,5 +5,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
+#include <string.h>
 
 #endif
