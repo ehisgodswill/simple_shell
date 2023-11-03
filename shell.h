f0da553dcc8aa1b823b8eba53ae1c4c3efe0eb9a
ehisgodswill <ehisgodswill@gmail.com>
2023-11-03 18:53:26 +0100

    description for structure

--- shell.h
+++ shell.h
@@ -7,6 +7,13 @@ extern char **environ;
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * struct Command - struct that holds all relevant command and argument
+ * @name: name of the command
+ * @arguments: array of argument vector
+ * @input_file: input fd
+ * @output_file: output fd
+ */
 typedef struct
 {
 	char *name;
