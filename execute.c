71dff5b14eab82b52b556584cf05de47a68f8811
ehisgodswill <ehisgodswill@gmail.com>
2023-11-03 19:24:28 +0100

    refactor execute.c

--- execute.c
+++ execute.c
@@ -1,21 +1,47 @@
 #include <sys/wait.h>
 #include "shell.h"
+
 /**
  * exec_line - executes the command
- * @cmd - pointer to command
- * Return: Nothing
-*/
+ * @cmd: pointer to command
+ */
+void exec_line(Command *cmd)
+{
+	char *path, *path_copy, *arguments[2], *token, command_path[1024];
 
+	if (strchr(cmd->name, '/') == NULL)
+	{
+		path = getenv("PATH");
+		if (path != NULL)
+		{
+			path_copy = strdup(path);
+			token = strtok(path_copy, ":");
+			while (token != NULL)
+			{
+				snprintf(command_path, sizeof(command_path), "%s/%s", token, cmd->name);
+				arguments[0] = command_path;
+				arguments[1] = NULL;
+				execve(command_path, arguments, environ);
+				token = strtok(NULL, ":");
+			}
+			free(path_copy);
+		}
+	}
+	else
+	{
+		arguments[0] = cmd->name;
+		arguments[1] = NULL;
+		execve(cmd->name, arguments, environ);
+	}
+}
 
 /**
  * execute_command - executes commands and fork the child process
  * @cmd: pointer to command
- * Return: void
  */
 void execute_command(Command *cmd)
 {
 	pid_t child_pid;
-	char *path, *path_copy, *arguments[2], *token, command_path[1024];
 
 	if (cmd->name == NULL)
 		return;
@@ -34,35 +60,10 @@ void execute_command(Command *cmd)
 			dup2(cmd->output_file, STDOUT_FILENO);
 			close(cmd->output_file);
 		}
-		if (strchr(cmd->name, '/') == NULL)
-		{
-			path = getenv("PATH");
-			if (path != NULL)
-			{
-				path_copy = strdup(path);
-				token = strtok(path_copy, ":");
-				while (token != NULL)
-				{
-					snprintf(command_path, sizeof(command_path), "%s/%s", token, cmd->name);
-					arguments[0] = command_path;
-					arguments[1] = NULL;
-					execve(command_path, arguments, environ);
-					token = strtok(NULL, ":");
-				}
-				free(path_copy);
-			}
-		}
-		else
-		{
-			arguments[0] = cmd->name;
-			arguments[1] = NULL;
-			execve(cmd->name, arguments, environ);
-		}
+		exec_line(&cmd);
 		perror("execve");
 		exit(1);
 	}
 	else
 		wait(NULL);
 }
-
-
