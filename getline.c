54b4ca59e0bd255b0d2c3955ee460faf590d9339
ehisgodswill <ehisgodswill@gmail.com>
2023-11-04 13:19:25 +0100

    getline

--- getline.c
+++ getline.c
@@ -5,22 +5,23 @@
  * @buffer: buffer to store line read
  * @position: position of buffer
  * @stream: file stream
- * 
+ *
  * Return: -1 on failure
 */
-int readline(char *buffer, int *position, FILE *stream)
+ssize_t readline(char *buffer, size_t *position, FILE *stream)
 {
 	char c;
-	int rd, pos = *position;
-	do
-	{
+	ssize_t nread = 0;
+	int rd;
+	size_t pos = *position;
+
+	do{
 		if (pos == 0)
 			fflush(stream);
 
 		rd = read(STDIN_FILENO, &c, 1);
-		if (rd == -1 || (pos== 0 && rd == 0))
+		if (rd == -1 || (pos == 0 && rd == 0))
 		{
-			free(buffer);
 			*position = pos;
 			return (-1);
 		}
@@ -28,6 +29,7 @@ int readline(char *buffer, int *position, FILE *stream)
 			buffer = realloc(buffer, pos + 1);
 		if (pos > 0 && (rd == 0 || rd == -1))
 		{
+			nread++;
 			pos++;
 			break;
 		}
@@ -37,7 +39,7 @@ int readline(char *buffer, int *position, FILE *stream)
 
 	buffer[pos] = '\0';
 	*position = pos;
-	return (rd);
+	return (nread);
 }
 
 /**
@@ -51,7 +53,8 @@ int readline(char *buffer, int *position, FILE *stream)
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
 	static char *buffer;
-	int position, rd;
+	ssize_t rd;
+	size_t position; 
 
 	buffer = malloc(sizeof(char) * BUFSIZ);
 	if (buffer == NULL)
@@ -59,7 +62,10 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	position = 0;
 	rd = readline(buffer, &position, stream);
 	if (rd == -1)
+	{
+		free(buffer);
 		return (-1);
+	}
 
 	if (*lineptr == NULL || position > *n)
 	{
