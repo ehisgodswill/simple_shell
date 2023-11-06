845b87eeccbe12f0f8f6631110d46aa05f15f00e
ehisgodswill <ehisgodswill@gmail.com>
2023-11-06 23:07:46 +0100

    setenv

--- env.c
+++ env.c
@@ -30,7 +30,7 @@ void set_environment(Command *cmd)
 	cmd->arguments[2] = NULL;
 	 execute_command(cmd); 
 	printf("%s %s %s %s", cmd->name, cmd->arguments[1], cmd->arguments[2], arg);*/
-	for (i = 1; i < 6; i++)
+	for (i = 1; cmd->arguments[i]; i++)
 	{
 		printf("'%s'\t", cmd->arguments[i]);
 	}
