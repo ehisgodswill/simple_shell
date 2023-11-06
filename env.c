4632b8f98da92ff8296382d57ead0860a7932f5b
ehisgodswill <ehisgodswill@gmail.com>
2023-11-06 20:50:36 +0100

    setenv

--- env.c
+++ env.c
@@ -23,7 +23,7 @@ void set_environment(Command *cmd)
 	char *arg = _strcat(cmd->arguments[1], cmd->arguments[2]);
 
 	cmd->name = "export";
-	cmd->arguments[1] = *arg;
+	cmd->arguments[1] = arg;
 	cmd->arguments[2] = NULL;
 	printf("%s %s %s", cmd->name, cmd->arguments[1], cmd->arguments[2]);
 	execute_command(cmd);
