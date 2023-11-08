20f474804108c2ede9ceb89b37ad8a7ff507e9a6
Emma Kiloko <kilokoemma175@gmail.com>
2023-11-08 01:04:58 -0800

    change directory

---
+++ cd.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+/**
+ * cd_command - changes the current directory, interpretes cd without arguments
+ * as cd $HOME, handles cd - and updates the PWD
+ * @cmd: pointer
+ */
+void cd_command(Command cmd)
+{
+	char *target_dir = NULL;
+
+	if (cmd->arguments[1] == NULL)
+		target_dir = getenv("HOME");
+	else if (_strcmp(cmd->arguments[1], "-") == 0)
+	{
+		target_dir = getenv("OLDPWD");
+		if (target_dir == NULL)
+		{
+			fprintf(stderr, "cd: OLDPWD not set\n");
+			return;
+		}
+	}
+	else
+		taget_dir = cmd->arguments[1];
+	if (chdir(target_dir) != 0)
+	{
+		perror("cd");
+		return;
+	}
+	char *current_dir = getcwd(NULL, 0);
+
+	if (current_dir == NULL)
+	{
+		perror("cd");
+		return;
+	}
+	if (setenv("PWD", current_dir, 1) != 0)
+		perror("cd");
+	if (setenv("OLDPWD", getenv("PWD"), 1) != 0)
+		perror("cd");
+	free(current_dir);
+}
